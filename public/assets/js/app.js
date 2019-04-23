bbe5939a7ace419539f7843d7329267464c25092
enfenry <enfenry@vt.edu>
2019-04-23 14:03:53 -0400

    styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles").append("<p data-id='" + data[i]._id + "'><a href=" + data[i].link + ">" + data[i].title + '</a><i class="fas fa-comment-alt comment"></i><br />' + "</p>");
     }
   });
   
